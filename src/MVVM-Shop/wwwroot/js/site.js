a777f85012359c9fb67df3878facf09e187205e7
keitkalon <79155265+keitkalon@users.noreply.github.com>
2021-11-18 16:41:57 +0200

    take an option on javascript

--- src/MVVM-Shop/wwwroot/js/site.js
+++ src/MVVM-Shop/wwwroot/js/site.js
@@ -10,3 +10,26 @@ async function AddToCart(ProductId) {
         window.location.pathname = "/Login";
     }
 }
+
+
+function checkAddress() {
+    const selection = document.getElementById('selectAdressCheckout');
+    var option5 = document.createElement('option');
+   
+    option5.innerHTML = "Dumbrava5";
+    selection.appendChild(option5)
+
+    const firstnameField = document.getElementById('firstNameOnCheckout');
+    const lastnameField = document.getElementById('lastNameOnCheckout');
+    const cityField = document.getElementById('cityOnCheckout');
+    const addressField = document.getElementById('addressOnCheckout');
+    const phoneField =  document.getElementById('phoneOnCheckout'); 
+    console.log(selection);
+    selection.addEventListener('click', event => {
+        alert(selection.options.text);
+    });
+}
+
+
+checkAddress();
+
